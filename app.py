91894587bef15d5d5549761ec64325c454191600
Diego Dukão <diegodukao@gmail.com>
2015-05-09 19:05:51 -0300

    second version of the rest api example

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
 #!flask/bin/python
-from flask import Flask, jsonify, abort, make_response, request, url_for
+from flask import Flask, jsonify, abort, make_response
+from flask.ext.restful import Api, Resource, reqparse, fields, marshal
 from flask.ext.httpauth import HTTPBasicAuth
-auth = HTTPBasicAuth()
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="")
+api = Api(app)
+auth = HTTPBasicAuth()
+#app = Flask(__name__)
+
+@auth.get_password
+def get_password(username):
+    if username == 'diego':
+        return 'python'
+    return None
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 403)
 
 tasks = [
     {
@@ -20,83 +37,77 @@ tasks = [
     }
 ]
 
-@app.route('/nostra-plata/api/v1.0/tasks', methods=['GET'])
-@auth.login_required
-def get_tasks():
-    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
-    
-@app.route('/nostra-plata/api/v1.0/tasks/<int:task_id>', methods=['GET'])
-def get_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    return jsonify({'task': task[0]})
-    
-@app.route('/nostra-plata/api/v1.0/tasks', methods=['POST'])
-def create_task():
-    if not request.json or not 'title' in request.json:
-        abort(400)
-    task = {
-        'id': tasks[-1]['id'] + 1,
-        'title': request.json['title'],
-        'description': request.json.get('description', ""),
-        'done': False
-    }
-    tasks.append(task)
-    return jsonify({'task': task}), 201
-    
-@app.route('/nostra-plata/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
-def update_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    if not request.json:
-        abort(400)
-    if 'title' in request.json and type(request.json['title']) != unicode:
-        abort(400)
-    if 'description' in request.json and type(request.json['description']) is not unicode:
-        abort(400)
-    if 'done' in request.json and type(request.json['done']) is not bool:
-        abort(400)
-    task[0]['title'] = request.json.get('title', task[0]['title'])
-    task[0]['description'] = request.json.get('description', task[0]['description'])
-    task[0]['done'] = request.json.get('done', task[0]['done'])
-    return jsonify({'task': task[0]})
+task_fields = {
+    'title': fields.String,
+    'description': fields.String,
+    'done': fields.Boolean,
+    'uri': fields.Url('task')
+}
 
-@app.route('/nostra-plata/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
-def delete_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    tasks.remove(task[0])
-    return jsonify({'result': True})
-    
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
-    
-@auth.error_handler
-def unauthorized():
-    return make_response(jsonify({'error': 'Unauthorized access'}), 403)
-    
-@auth.get_password
-def get_password(username):
-    if username == 'diego':
-        return 'python'
-    return None
+class TaskListAPI(Resource):
+    decorators = [auth.login_required]
 
-@auth.error_handler
-def unauthorized():
-    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
-    
-def make_public_task(task):
-    new_task = {}
-    for field in task:
-        if field == 'id':
-            new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
-        else:
-            new_task[field] = task[field]
-    return new_task
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('title', type=str, required=True,
+                                   help='No task title provided',
+                                   location='json')
+        self.reqparse.add_argument('description', type=str, default="",
+                                   location='json')
+        super(TaskListAPI, self).__init__()
+
+    def get(self):
+        return {'tasks': [marshal(task, task_fields) for task in tasks]}
+
+    def post(self):
+        args = self.reqparse.parse_args()
+        task = {
+            'id': tasks[-1]['id'] + 1,
+            'title': args['title'],
+            'description': args['description'],
+            'done': False
+        }
+        tasks.append(task)
+        return {'task': marshal(task, task_fields)}, 201
+
+
+class TaskAPI(Resource):
+    decorators = [auth.login_required]
+
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('title', type=str, location='json')
+        self.reqparse.add_argument('description', type=str, location='json')
+        self.reqparse.add_argument('done', type=bool, location='json')
+        super(TaskAPI, self).__init__()
+
+    def get(self, id):
+        task = [task for task in tasks if task['id'] == id]
+        if len(task) == 0:
+            abort(404)
+        return {'task': marshal(task[0], task_fields)}
+
+    def put(self, id):
+        task = [task for task in tasks if task['id'] == id]
+        if len(task) == 0:
+            abort(404)
+        task = task[0]
+        args = self.reqparse.parse_args()
+        for k, v in args.items():
+            if v is not None:
+                task[k] = v
+        return {'task': marshal(task, task_fields)}
+
+    def delete(self, id):
+        task = [task for task in tasks if task['id'] == id]
+        if len(task) == 0:
+            abort(404)
+        tasks.remove(task[0])
+        return {'result': True}
+
+
+api.add_resource(TaskListAPI, '/nostra-plata/api/v1.0/tasks', endpoint='tasks')
+api.add_resource(TaskAPI, '/nostra-plata/api/v1.0/tasks/<int:id>', endpoint='task')
     
 if __name__ == '__main__':
     app.run(debug=True)
