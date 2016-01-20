ceddc66690c709c274fd49a66e2549c809cf38fc
Diego Dukão <diegodukao@gmail.com>
2016-01-20 15:06:59 -0200

    (auth) adding username and password field in User model

--- app/models.py
+++ app/models.py
@@ -1,9 +1,11 @@
+from passlib.apps import custom_app_context as pwd_context
 from app import db
 
 group_members = db.Table('group_members',
     db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
     db.Column('group_id', db.Integer, db.ForeignKey('group.id')),
 )
+
 
 class Loan(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,16 +17,27 @@ class Loan(db.Model):
     def __repr__(self):
         return '<Loan: %r to %r: %r>' % (self.creditor.name, self.debtor.name, self.amount)
 
+
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
     name = db.Column(db.String(120), index=True)
     loans_given = db.relationship('Loan', backref=db.backref('creditor'),
-                              primaryjoin=id==Loan.creditor_id)
+                                  primaryjoin=id == Loan.creditor_id)
     loans_gotten = db.relationship('Loan', backref=db.backref('debtor'),
-                               primaryjoin=id==Loan.debtor_id)
+                                   primaryjoin=id == Loan.debtor_id)
 
     def __repr__(self):
-        return '<User %r>' % (self.name)
+        return '<User %r>' % (self.username)
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
 
 class Group(db.Model):
     id = db.Column(db.Integer, primary_key=True)
